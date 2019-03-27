c60c50b0a31d0168e9556f908c5cbb8cb188df5a
GalIfrahh <48998366+GalIfrahh@users.noreply.github.com>
2019-03-28 00:15:07 +0200

    Update ConnectTests.py

--- Tests/ConnectTests.py
+++ Tests/ConnectTests.py
@@ -22,33 +22,34 @@ class ConnectTests(BaseTest):
           self.assertTrue(current_login_button_text != before_login_button_text, ErrorsHandler.LOGIN_ERROR + " " +
                           current_login_button_text)
     
-    # def test_101_login(self):
-    #
-    #     Connect.login(self)
-    #
-    #     current_login_button_text = HomePage.getLoginButtonText(self)
-    #
-    #     before_login_button_text = config['HOME_PAGE']['TEXTS']['CONNECT_BUTTON_BEFORE_LOGIN']
-    #
-    #     assert current_login_button_text != before_login_button_text
-    #
-    # def test_102_logout(self):
-    #
-    #     # login
-    #     Connect.login(self)
-    #
-    #     current_login_button_text = HomePage.getLoginButtonText(self)
-    #
-    #     before_login_button_text = config['HOME_PAGE']['TEXTS']['CONNECT_BUTTON_BEFORE_LOGIN']
-    #
-    #     assert current_login_button_text != before_login_button_text
-    #
-    #     # logout
-    #     Connect.logout(self)
-    #
-    #     current_login_button_text = HomePage.getLoginButtonText(self)
-    #
-    #     assert current_login_button_text == before_login_button_text
-    #
-    # def test_103_checkMigration(self):
-    #     pass
+    
+    def test_101_login(self):
+    
+         Connect.login(self)
+    
+         current_login_button_text = HomePage.getLoginButtonText(self)
+    
+         before_login_button_text = config['HOME_PAGE']['TEXTS']['CONNECT_BUTTON_BEFORE_LOGIN']
+    
+         assert current_login_button_text != before_login_button_text
+    
+    
+    def test_102_logout(self):
+    
+         # login
+         Connect.login(self)
+    
+         current_login_button_text = HomePage.getLoginButtonText(self)
+    
+         before_login_button_text = config['HOME_PAGE']['TEXTS']['CONNECT_BUTTON_BEFORE_LOGIN']
+    
+         assert current_login_button_text != before_login_button_text
+    
+         # logout
+         Connect.logout(self)
+    
+         current_login_button_text = HomePage.getLoginButtonText(self)
+    
+         assert current_login_button_text == before_login_button_text
+    
+    
