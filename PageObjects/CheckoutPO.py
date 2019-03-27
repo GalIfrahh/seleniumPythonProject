b41596f83bb8a71684aa6e7e82efda31695b42bd
GalIfrahh <48998366+GalIfrahh@users.noreply.github.com>
2019-03-27 23:49:40 +0200

    Update CheckoutPO.py

--- PageObjects/CheckoutPO.py
+++ PageObjects/CheckoutPO.py
@@ -4,27 +4,26 @@ from Infrastracture.GenericPageObjects import *
 class Checkout:
 
 
-
     def addPaymentFromCheckout(self):
-        GenericPO.driver.findElementBy(self, "//div[@class='add-payment-method ng-binding ng-scope']",
+        GenericPO.driver.findElementBy(self, config['CHECKOUT_SCREEN']['LOCATORS']['LOADER'],
                                           By.XPATH)
 
 
     def clickOnSubmitOrder(self):
-        if GenericPO.driver.waitForInvisibilityOfElem(self, "//div[@class='loading']") is True:
+        if GenericPO.driver.waitForInvisibilityOfElem(self, config['CHECKOUT_SCREEN']['LOCATORS']['LOADER']) is True:
                 GenericPO.driver.waitForElemToBeClickable(config['CHECKOUT_SCREEN']['LOCATORS']['SUBMIT_ORDER_BUTTON'])
 
 
 
     def enter4DigitsCode(self):
         GenericPO.driver.findElementBy(self, config['CHECKOUT_SCREEN']['LOCATORS']['ENTER_PIN_INPUT'],
-                                    By.XPATH).send_keys(
-            config['FORM_PAGE']['DATA']['PIN'])
+                                    By.XPATH).send_keys(config['FORM_PAGE']['DATA']['PIN'])
 
+        
+        
     def submit4digitsCode(self):
         GenericPO.driver.findElementBy(self, config['CHECKOUT_SCREEN']['LOCATORS']['POPUP_OK_BTN'],
                                           By.XPATH).click()
-        time.sleep(2)
 
 
     def Pass4DigitsPin(self):
@@ -37,6 +36,7 @@ class Checkout:
             else:
                   pass
 
+                
     def getErrorPopup(self):
         pop_up_element = GenericPO.driver.waitForVisibilityOfElem(self, config['CHECKOUT_SCREEN']['LOCATORS']['CHECKOUT_POPUP'])
 
@@ -46,8 +46,8 @@ class Checkout:
     def clickOnPopUpOkBtn(self):
         GenericPO.driver.findElementBy(self, config['CHECKOUT_SCREEN']['LOCATORS']['POPUP_OK_BTN'], By.XPATH).click()
 
-        time.sleep(1)
 
+        
     def getErrorPopupText(self):
         text = GenericPO.driver.waitForVisibilityOfElem(self, config['CHECKOUT_SCREEN']['LOCATORS']['CHECKOUT_POPUP']).text
 
