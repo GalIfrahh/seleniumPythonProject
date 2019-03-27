240ff5f70a2857c3d919adb139bdc295837b34a4
GalIfrahh <48998366+GalIfrahh@users.noreply.github.com>
2019-03-27 23:56:04 +0200

    Update HomePO.py

--- PageObjects/HomePO.py
+++ PageObjects/HomePO.py
@@ -9,7 +9,6 @@ class HomePage(GenericPO):
                                     By.XPATH).click()
 
 
-
     def openSut(self, env):
 
         if env == "test":
@@ -17,44 +16,44 @@ class HomePage(GenericPO):
 
         elif env == "sand":
             GenericPO.driver.openSut(self, config['SUT']['SAND'])
-            time.sleep(2)
+            
 
         elif env == "prod":
             GenericPO.driver.openSut(self, config['SUT']['PROD'])
-            time.sleep(2)
 
 
-
-
-
+            
     def getSutUrl(self):
         text = GenericPO.driver.getCurrentUrl(self)
 
         return text
 
+    
 
     def getCookPolicyTxt(self):
         txt = GenericPO.driver.findElementBy(self, config['HOME_PAGE']['LOCATORS']['COOKIES_POLICY_BTN'],
                                                 By.XPATH).text
         return txt
 
-    def goToAppSite_header(self):
+    
+    def goToAppSiteHeader(self):
         GenericPO.driver.findElementBy(self, config['HOME_PAGE']['LOCATORS']['BACK_TO_APP_HEADER_LINK'],
                                           By.XPATH).click()
 
+        
     def getAppLinkText(self):
         app_link_text = GenericPO.driver.findElementBy(self, config['HOME_PAGE']['LOCATORS']['BACK_TO_APP_HEADER_LINK'],
                                                        By.XPATH).text
         return app_link_text
 
 
+    
     def clickOnConnect(self):
         GenericPO.driver.findElementBy(self, config['HOME_PAGE']['LOCATORS']['CONNECT_BTN'],
                                     By.ID).click()
 
 
     def getLoginButtonText(self):
-        time.sleep(3)
         text = GenericPO.driver.findElementBy(self, config['HOME_PAGE']['LOCATORS']['CONNECT_BTN_TEXT_AREA'],
                                                  By.XPATH).text
         return text
@@ -70,13 +69,15 @@ class HomePage(GenericPO):
 
             GenericPO.driver.findElementBy(self, config['HOME_PAGE']['LOCATORS']['SELECT_TIME_PLACE_HOLDER'],
                                            By.XPATH).text]
+        
         return place_holders
 
+    
+    
     def chooseLocation(self):
         GenericPO.driver.selectFromDropDown(self, config['HOME_PAGE']['LOCATORS']['SELECT_LOCATION_DROP_DOWN'],
                                             config['HOME_PAGE']['DATA']['SECOND_LOCATION'])
 
-        time.sleep(2)
 
     def getLocationsList(self):
         locations_list = GenericPO.driver.getDropDownOptionsList(self,
@@ -84,6 +85,8 @@ class HomePage(GenericPO):
 
         return locations_list
 
+    
+    
     def chooseDate(self):
         GenericPO.driver.selectFromDropDown(self, config['HOME_PAGE']['LOCATORS']['SELECT_DATE_DROP_DOWN'],
                                         config['HOME_PAGE']['DATA']['TOMORROW'])
@@ -93,58 +96,7 @@ class HomePage(GenericPO):
         GenericPO.driver.selectFromDropDown(self, config['HOME_PAGE']['LOCATORS']['SELECT_TIME_DROP_DOWN'],
                                         config['HOME_PAGE']['DATA']['TIME'])
 
-    def startOrder(self, test_location_number):
-        if test_location_number == 1:
-            GenericPO.driver.selectFromDropDown(self, config['HOME_PAGE']['LOCATORS']['SELECT_LOCATION_DROP_DOWN'],
-                                                config['HOME_PAGE']['DATA']['FIRST_LOCATION_NOT_WORKING'])
-
-        if test_location_number == 2:
-            time.sleep(1)
-            GenericPO.driver.selectFromDropDown(self, config['HOME_PAGE']['LOCATORS']['SELECT_LOCATION_DROP_DOWN'],
-                                                config['HOME_PAGE']['DATA']['SECOND_LOCATION'])
-
-        time.sleep(2)
-        GenericPO.driver.waitForElemToBeClickable(self, config['HOME_PAGE']['LOCATORS']['START_ORDER_BUTTON'])
-
-        time.sleep(3)
-        if GenericPO.driver.getCurrentUrl(self) == config['MENU']['LOCATORS']['MENU_URL']:
-            pass
-
-
-        elif HomePage.getStartOrderPopup(self) is not None:
-
-            if HomePage.getStartOrderPopup(self).text == config['HOME_PAGE']['TEXTS'][
-                    'START_ORDER_CHOOSE_LOCATION_POPUP_TEXT']:
-
-                HomePage.clickOnStartOrderPopupButton(self)
-
-                HomePage.chooseLocation(self)
-
-                HomePage.startOrder(self, 1)
-
-            elif HomePage.getStartOrderPopup().text == config['HOME_PAGE']['TEXTS'][
-                    'START_ORDER_TIME_EXCEEDED_POPUP_TEXT']:
-
-                HomePage.clickOnStartOrderPopupButton(self)
-
-                HomePage.startOrder(self, 2)
-
-            elif HomePage.getStartOrderPopup(self).text == config['HOME_PAGE']['TEXTS'][
-                    'START_ORDER_NOT_ACCEPTING_POPUP_TEXT']:
-
-                HomePage.clickOnStartOrderPopupButton(self)
-
-                HomePage.startOrder(2)
-
-            elif HomePage.getStartOrderPopup(self).text == config['HOME_PAGE']['TEXTS'][
-                    'START_ORDER_SELECT_TIME_POPUP_TEXT']:
-
-                HomePage.clickOnStartOrderPopupButton(self)
-
-                HomePage.chooseLocation(self)
-
-                HomePage.startOrder(self, 2)
-
+        
     def getStartOrderPopup(self):
         element = GenericPO.driver.findElementBy(self, config['HOME_PAGE']['LOCATORS']['START_ORDER_POPUP_TEXT_AREA'],
                                     By.XPATH)
@@ -156,6 +108,7 @@ class HomePage(GenericPO):
                                     By.XPATH).click()
 
 
+        
     def clickOnTermsAndConditions(self):
         GenericPO.driver.findElementBy(self, config['HOME_PAGE']['LOCATORS']['TERMS_AND_COND_BUTTON'],
                                     By.XPATH).click()
