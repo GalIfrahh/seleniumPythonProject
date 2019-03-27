b9440baff56daab497469f3771b2fd46eee8977d
GalIfrahh <48998366+GalIfrahh@users.noreply.github.com>
2019-03-28 00:23:01 +0200

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -61,7 +61,6 @@ def platform(request):
 def pytest_runtest_makereport(item):
     """
     Extends the PyTest Plugin to take and embed screenshot in html report, whenever test fails.
-    :param item:
     """
     pytest_html = item.config.pluginmanager.getplugin('html')
     outcome = yield
