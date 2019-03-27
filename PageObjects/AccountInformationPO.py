3f95e2ea3a4014f20bcf38506c8b1f8d0009e182
GalIfrahh <48998366+GalIfrahh@users.noreply.github.com>
2019-03-27 23:44:21 +0200

    Delete AccountInformationPO.py

--- PageObjects/AccountInformationPO.py
+++
@@ -1,9 +0,0 @@
-from Infrastracture.GenericPageObjects import *
-
-
-class AccountInformation:
-
-    def getNameText(self):
-        text = GenericPO.driver.findElementBy(self, "//*[@id='modal-body']/div/div[1]/div/input",
-                                              By.XPATH).text
-        return text
